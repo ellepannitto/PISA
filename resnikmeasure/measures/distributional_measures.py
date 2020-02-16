da48a58e01721902af4377a1917e192ede1dfdc5
ludovica <ellepannitto@gmail.com>
2020-02-17 00:19:17 +0100

    better multiprocessing for cosines

--- resnikmeasure/measures/distributional_measures.py
+++ resnikmeasure/measures/distributional_measures.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
 from scipy.spatial.distance import cosine
-import collections
+import itertools
 import tqdm
 from multiprocessing import Pool
+import functools
 
 def f(verb, output_path, algo, folder, nouns_per_verb, noun_vectors):
     print("START", verb, output_path, algo, folder)
@@ -15,9 +16,18 @@ def f(verb, output_path, algo, folder, nouns_per_verb, noun_vectors):
                     print(noun1, noun2, cos, file=fout)
     print("END", verb, output_path, algo, folder)
 
-def parallel_f(tup):
-    return f(*tup)
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
+def parallel_f(noun_vectors, tup):
+#    noun_vectors, tup_list = tup
+    tup_list = filter(lambda x: x is not None, tup)
+    #print(os.getpid(), "START")
+    #return [(n1, n2, 10) for n1, n2 in tup_list]
+    return [(n1, n2, 1-cosine(noun_vectors[n1], noun_vectors[n2])) for n1, n2 in tup_list]
 
 def compute_measure(input_path, output_path, nouns_file, n_workers):
     os.makedirs(output_path, exist_ok=True)
@@ -61,10 +71,17 @@ def compute_measure(input_path, output_path, nouns_file, n_workers):
                 v = noun_vectors[n][-min_len:]
                 noun_vectors[n] = np.array(v)
 
-
-            lista_verbi = [(verb, output_path, algo, folder, nouns_per_verb, noun_vectors) for verb in nouns_per_verb.keys()]
-            with Pool(n_workers) as p:
-                p.map(parallel_f, lista_verbi)
+            for verb in nouns_per_verb.keys():
+                print("PROCESSING VERB ", verb)
+                with Pool(n_workers) as p:
+                    with open(output_path + algo + "." + folder + "." + verb, "w") as fout:
+                        iterator = grouper(itertools.combinations(nouns_per_verb[verb], 2), 1000000)
+                        tot = (len(nouns_per_verb[verb])*(len(nouns_per_verb[verb]) - 1))//(2*1000000)
+                        for retlist in tqdm.tqdm(p.imap(functools.partial(parallel_f, noun_vectors), iterator), total=tot+1):
+                            for n1, n2, cos in retlist:
+                                print(n1, n2, cos, file=fout)
+    #                        print("RICEVUTI RISULTATI")
+    #                        input()
 
 
     for algo in ["SVD"]:
@@ -90,6 +107,15 @@ def compute_measure(input_path, output_path, nouns_file, n_workers):
                 v = noun_vectors[n][-min_len:]
                 noun_vectors[n] = np.array(v)
 
-            lista_verbi = [(verb, output_path, algo, folder, nouns_per_verb, noun_vectors) for verb in nouns_per_verb.keys()]
-            with Pool(n_workers) as p:
-                p.map(parallel_f, lista_verbi)
+                for verb in nouns_per_verb.keys():
+                    print("PROCESSING VERB ", verb)
+                    with Pool(n_workers) as p:
+                        with open(output_path + algo + "." + folder + "." + verb, "w") as fout:
+                            iterator = grouper(itertools.combinations(nouns_per_verb[verb], 2), 1000000)
+                            tot = (len(nouns_per_verb[verb]) * (len(nouns_per_verb[verb]) - 1)) // (2 * 1000000)
+                            for retlist in tqdm.tqdm(p.imap(functools.partial(parallel_f, noun_vectors), iterator),
+                                                     total=tot + 1):
+                                for n1, n2, cos in retlist:
+                                    print(n1, n2, cos, file=fout)
+        #                        print("RICEVUTI RISULTATI")
+        #                        input()
